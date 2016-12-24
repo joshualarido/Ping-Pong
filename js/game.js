351566e44f0a1117a81ba79a0fbd7bbc80683213
joshualarido <joshualarido@gmail.com>
2016-12-25 02:10:01 +0700

    first commit

---
+++ js/game.js
@@ -0,0 +1,11 @@
+var game = new Phaser.Game(640, 400, Phaser.AUTO, 'gameDiv');
+
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('menu', menuState);
+game.state.add('play', playState);
+game.state.add('restart', restartState);
+game.state.add('winp1', winP1State);
+game.state.add('winp2', winP2State)
+
+game.state.start('boot');
---
+++ js/load.js
@@ -0,0 +1,15 @@
+var loadState = {
+	preload: function() {
+		var loadingLabel = game.add.text(80, 150, 'loading...', {font: '30 px Courier', fill: "#00de45"});
+		game.load.image('Ball', 'assets/soccer-ball.png')
+		game.load.image('Background', 'assets/pixelBG.jpg')
+		game.load.audio('Hit', 'assets/silencer.wav')
+		game.load.audio('Bounce', 'assets/Bounce.wav')
+
+
+	},
+
+	create: function() {
+		game.state.start('menu');
+	}
+}
